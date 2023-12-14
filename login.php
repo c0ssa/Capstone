03d25ab4fd105bad3be5ceab0d30a56a5a567ebc
c0ssa <125482640+c0ssa@users.noreply.github.com>
2023-12-14 05:43:01 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Form Elements</title>
+    <meta charset="utf-8">
+    <link href="homePageLayout.css" rel="stylesheet">
+<main>
+</head>
+<header>
+    <h1>Major League Baseball Live Updates</h1>
+    <nav>
+      <ul>
+        <li><a href="homePage.php">Home</a></li>
+        <li><a href="teams.php">Team Rosters</a></li>
+        <li><a href="standings.php">Standings</a></li>
+        <li><a href="login.php">Login to MLB</a></li>
+      </ul>
+    </nav>
+  </header><br>
+
+    <h2>Welcome to the MLB Game Vault!</h2>
+<body>
+        <h3>What is your role?</h3>
+        <a href="favoriteTeam.php">Fan</a>
+        <br>
+        <a href="desiredTeam.php">Statistician</a>
+        <br>
+        <br>
+</form>
+<footer>
+    <p>&copy; 2023 MLB Live Updates</p>
+  </footer>
+</body>
+</body>
+</main>
+</html>
